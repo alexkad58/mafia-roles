98d72cf838ebe103a71c89435a84306db00c7d5b
alexkad58 <>
2021-04-30 19:17:14 +0000

    a

--- frontend/index.js
+++ frontend/index.js
@@ -27,15 +27,58 @@ document.addEventListener('DOMContentLoaded', function() {
     let queueString = ''
     let arr = [1,2,3,4,5,6,7,8,9,10]
     shuffle(arr)
+
+    const maf = document.getElementById(`maf`)
+    const kom = document.getElementById(`kom`)
+    const mir = document.getElementById(`mir`)
+ 
     arr.forEach((e, i) => {
       console.log(rolesArr, rolesArr[i] )
       queueEl = document.getElementById(`${i + 1}`)
       queueEl.innerHTML = ` ${e} `;
       queueEl.style.fontWeight = "700"
-      if (rolesArr[i] == 3) queueEl.style.color = 'red'
-      if (rolesArr[i] == 2) queueEl.style.color = 'blue'
-      if (rolesArr[i] == 1) queueEl.style.color = '#28a745'
+      queueEl.style.fontSize = 'x-large'
+      if (rolesArr[i] == 4) {
+        queueEl.style.color = 'black'
+        text = maf.innerHTML
+        maf.style.color = 'black'
+        maf.style.fontWeight = "700"
+        maf.style.fontSize = 'x-large'
+        maf.style.margin = '10px'
+        maf.innerHTML = `${text} ${e}`
+      }
+      if (rolesArr[i] == 3) {
+        queueEl.style.color = 'red'
+        text = maf.innerHTML
+        maf.style.color = 'red'
+        maf.style.fontWeight = "700"
+        maf.style.fontSize = 'x-large'
+        maf.style.margin = '10px'
+        maf.innerHTML = `${text} ${e}`
+      }
+      if (rolesArr[i] == 2) {
+        queueEl.style.color = 'blue'
+        text = kom.innerHTML
+        kom.style.color = 'blue'
+        kom.style.fontWeight = "700"
+        kom.style.fontSize = 'x-large'
+        kom.style.margin = '10px'
+        kom.innerHTML = `${text} ${e}`
+      }
+      if (rolesArr[i] == 1) {
+        queueEl.style.color = '#28a745'
+        text = mir.innerHTML
+        mir.style.color = '#28a745'
+        mir.style.fontWeight = "700"
+        mir.style.fontSize = 'x-large'
+        mir.style.margin = '10px'
+        mir.innerHTML = `${text} ${e}`
+      }
       queueEl.style.display = 'inline'
+      queueEl.style.margin = '10px'
+      maf.style.wordSpacing = '10px'
+      kom.style.wordSpacing = '10px'
+      mir.style.wordSpacing = '10px'
       
     })
    
