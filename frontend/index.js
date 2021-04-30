91246bee2ae121534716f849228364d3c2d544d1
alexkad58 <>
2021-04-30 18:52:47 +0000

    ф

--- frontend/index.js
+++ frontend/index.js
@@ -8,11 +8,10 @@ const socket = io('https://mafia-roles.alexkad58.repl.co');
 const queueText = document.getElementById('queue')
 const rollButton = document.getElementById('roll')
 
-rollButton.on('click', getQueue())
+// rollButton.onclick(getQueue())
 
-function getQueue() {
-
-    function shuffle(a) {
+document.addEventListener('DOMContentLoaded', function() {
+  function shuffle(a) {
         var j, x, i;
         for (i = a.length - 1; i > 0; i--) {
             j = Math.floor(Math.random() * (i + 1));
@@ -22,14 +21,28 @@ function getQueue() {
         }
         return a;
     }
-
-    let rolesArr = ['маф','маф','маф','ком','мир','мир','мир','мир','мир','мир']
+    
+    let rolesArr = [4,3,3,2,1,1,1,1,1,1]
     shuffle(rolesArr)
     let queueString = ''
     let arr = [1,2,3,4,5,6,7,8,9,10]
     shuffle(arr)
     arr.forEach((e, i) => {
-        
-        queueText.innerHTML(`${e} `)
+      console.log(rolesArr, rolesArr[i] )
+      queueEl = document.getElementById(`${i + 1}`)
+      queueEl.innerHTML = ` ${e} `;
+      queueEl.style.fontWeight = "700"
+      if (rolesArr[i] == 3) queueEl.style.color = 'red'
+      if (rolesArr[i] == 2) queueEl.style.color = 'blue'
+      if (rolesArr[i] == 1) queueEl.style.color = '#28a745'
+      queueEl.style.display = 'inline'
+      
     })
+   
+}, false);
+
+
+function getQueue() {
+
+    
 }
